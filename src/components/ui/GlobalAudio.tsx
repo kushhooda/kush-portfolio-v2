1b7bed53ea16cc51a9ba812ce4290edd25c29cd5
kushhooda <kush9hooda@gmail.com>
2026-04-18 11:42:45 +0530

    Restrict audio click to buttons and links

--- src/components/ui/GlobalAudio.tsx
+++ src/components/ui/GlobalAudio.tsx
@@ -6,15 +6,17 @@ export default function GlobalAudio() {
   const audioRef = useRef<HTMLAudioElement>(null)
 
   useEffect(() => {
-    const handleClick = () => {
-      if (audioRef.current) {
-        // Reset the audio to start to allow rapid clicking
-        audioRef.current.currentTime = 0
-        audioRef.current.play().catch(e => {
-          // Browsers block autoplay until user interaction, 
-          // but since this is triggered by a click, it should usually succeed.
-          console.log("Audio playback prevented by browser:", e)
-        })
+    const handleClick = (e: MouseEvent) => {
+      const target = e.target as HTMLElement
+      if (target.closest('button') || target.closest('a')) {
+        if (audioRef.current) {
+          // Reset the audio to start to allow rapid clicking
+          audioRef.current.currentTime = 0
+          audioRef.current.play().catch(e => {
+            // Browsers block autoplay until user interaction
+            console.log("Audio playback prevented by browser:", e)
+          })
+        }
       }
     }
 
